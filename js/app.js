bcd11e3b09333a520b66b730e3849a41e7b22deb
Vip4rontender <vasyl.shushansky>
2023-03-26 21:43:52 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// function test() {
+//   return false;
+// }
+
+function validateEmail(email) {
+  // if (email === "") return false;
+  // if (typeof email !== "string") return false;
+  // if (!email.split("").includes("@") || email.split("")[0] === "@")
+  //   return false;
+  // if (email.length < 5) return false;
+  // if (email.split("@").length === 2) return false;
+
+  if (!email.split("@")[1].includes(".")) return false;
+  // if (email.split("@")[1].indexOf(".") === 0) return false;
+
+  if (email.lastIndexOf(".") < email.indexOf("@")) return false;
+  return true;
+}
---
+++ js/validateEmail.test.js
@@ -0,0 +1,43 @@
+// describe("describe", function () {
+//   it("test", function () {});
+// });
+
+let assert = chai.assert;
+
+describe("validateEmail", function () {
+  describe("valid string", function () {
+    it("not an empty string", function () {
+      assert.equal(validateEmail(""), false);
+    });
+    it("typeof string", function () {
+      assert.equal(validateEmail(222), false, "number error");
+      assert.equal(validateEmail(true), false, "boolean error");
+      assert.equal(validateEmail({}), false, "object error");
+    });
+    it("not all spaces", function () {
+      assert.equal(validateEmail(" "), false);
+    });
+  });
+  describe("valid data", function () {
+    it("Don't have the @ or @ the first character", function () {
+      assert.isFalse(validateEmail("emailgmail.com"), "Don't have the @");
+      assert.isFalse(validateEmail("@emailgmail.com"), "@ the first character");
+    });
+  });
+  describe("homeWork", function () {
+    it("length less than 5", function () {
+      assert.equal(validateEmail("emai"), false);
+    });
+    it("should contain only one symbol '@'", function () {
+      assert.equal(validateEmail("email@gmail"), false);
+    });
+    it("should contain '.'", function () {
+      assert.isTrue(validateEmail("e.mail@gmail.com"), "valid email");
+      assert.isFalse(validateEmail("e.mail@gmailcom"), `missing '.' after '@'`);
+      assert.isFalse(
+        validateEmail("e.mail.@gmail.com"),
+        `invalid '.' before '@'`
+      );
+    });
+  });
+});
